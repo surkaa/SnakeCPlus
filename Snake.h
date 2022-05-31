a2fb80879f64c7ec1148daa62be42373480475fc
winingYang <98200894+winingYang@users.noreply.github.com>
2022-05-31 15:38:43 +0800

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -9,33 +9,25 @@
 #include<time.h>
 #include<math.h>
 
-//Game
-#define mapSize	            900
-#define mapWidth            30
-#define mapHigh	            (mapSize / mapWidth)
+//Init Msp
+//#define mapSize	        1500		//蛇活动的格数(1500)
+//#define mapWidth          50			//蛇活动的宽度(50)
+#define mapSize	            1500		//蛇活动的格数
+#define mapWidth            50			//蛇活动的宽度
+#define cellWidth			20			//蛇身的宽度
+#define cellGap				3			//蛇身之间的间隙
+#define initlong			2			//蛇的初始长度
+#define PlayerSpeed			80			//玩家的屏幕刷新速度
+#define AISpeed			    0			//机器的屏幕刷新速度
+#define DrewStartPoint_x	30			//蛇图绘画起始点X
+#define DrewStartPoint_y	50			//蛇图绘画起始点Y
+
+#define SpeedRelationship	(log(PlayerSpeed) / log(2) / mapSize)
+#define mapHigh	            (mapSize / mapWidth)									
 #define pmapW               (mapWidth + 2)
 #define pmapH               (mapHigh + 2)
 #define pmapS		        (pmapW * pmapH)
-#define cellGap				3
-#define cellWidth			25
-#define initlong			15
-#define initspeed			1
 
-//Drew
-#define DrewStartPoint_x	3
-#define DrewStartPoint_y	50
-#define screenWidth			(2 * DrewStartPoint_x + pmapW * cellWidth)
-#define screenHigh			(2 * DrewStartPoint_y + pmapH * cellWidth)
-#define outputMessageA_x	(3 * screenWidth / 10)
-#define outputMessageB_x	(7 * screenWidth / 10)
-#define outputMessage__x    ((outputMessageA_x + outputMessageB_x) / 2) - 230
-#define outputMessageAB_y	(4 * DrewStartPoint_y / 5)
-#define SpeedRelationship	(log(initspeed) / log(2) / mapSize)
-#define MessageWidth		(DrewStartPoint_y / 2)
-#define MapBorderA_x		(DrewStartPoint_x + cellWidth)
-#define MapBorderA_y		(DrewStartPoint_y + cellWidth)
-#define MapBorderB_x		(DrewStartPoint_x + pmapW * cellWidth - cellWidth - cellGap)
-#define MapBorderB_y		(DrewStartPoint_y + pmapH * cellWidth - cellWidth - cellGap)
 
 //Dircetion
 #define unkown				0
@@ -44,7 +36,7 @@
 #define down				pmapW
 #define right				1
 
-//Map.adr
+//SNAKE.Attributes
 #define apple				YELLOW
 #define SnakeBody			BROWN
 #define none				LIGHTGRAY
@@ -52,9 +44,9 @@
 
 struct SnakeMap
 {
-	int adr;
-	int dir;
-	int way;
+	int Attributes;
+	int Direction;
+	int Number;
 };
 
 struct SnakeRelevant
@@ -67,52 +59,49 @@ struct SnakeRelevant
 	int condition;
 	int x_FoodHead;
 	int y_FoodHead;
-	int x_TailHead;
-	int y_TailHead;
 	int stepNumber;
-	double speed;
+	int speed;
 };
 
 //Desk
-char SELECT();
-void DEBUGAlpha(bool);
-void SnakeGame(bool);
-void HELP();
+char SELECT();											//采集信息
+void DEBUGAlpha(bool);									//调试游戏
+void GameStart(bool);									//开始游戏
+void HELP();											//游戏帮助
 
-//Drew
-void DISPLAY_DESK();
-void fnDrawSnakeBody(SnakeMap*, SnakeRelevant*);
-void fnOutputMessage(SnakeMap*, SnakeRelevant*);
+//Display
+void DISPLAY_DESK();									//描绘桌面
+void fnDrawSnakeBody(SnakeMap*, SnakeRelevant*);		//绘画蛇身
+void fnOutputMessage(SnakeRelevant*, bool);				//输出信息
 
 //CPU
-void InitGame();
-void AddApple();
-void TrunSnake(bool, bool);
-bool CheakSnakeSafety();
-int isSnakeBody(int);
-int CheakHtoTFSafety(int, int);
-bool update(int n);
-void around(int num, int nway);
-void DrewSnake(bool);
-void fnRefresh();
-void MoveSnake();
-void fnRefresh();
-double completeness();
+void InitMsp();											//地图初始化
+void AddApple();										//生成食物
+void TrunSnake(bool, bool);								//蛇的转向
+bool CheakSnakeSafety();								//检查此方向是否通行
+int isSnakeBody(int);									//是否能让蛇通行
+int CheakHtoTFSafety(int, int);							//检查某方向能否连通某地点
+bool fnNextCheck(int);								    //检查下一步
+void fnAroundCheak(int, int);					        //检查下一步的周围
+void fnDisplaySnake(bool);								//绘图
+void fnRefresh();										//更新数据
+void MoveSnake();										//移动蛇
+double debuggingResults();								//返回此次调试成绩
 
 //Al
-int TrunSnake_byAlpha(SnakeMap*, SnakeRelevant*);
-int fnAsFarAsThePathtoFood(SnakeRelevant*);
-int fnAsFarAsThePathtoFood_(SnakeRelevant*);
-int fnAsFarAsThePathtoTail_(SnakeRelevant*);
+int TrunSnake_byAlpha(SnakeMap*, SnakeRelevant*);		//通过机器转向
+int fnFarthestToTheSnakeFood_A(SnakeRelevant*);			//远离食物方案A
+int fnFarthestToTheSnakeFood_B(SnakeRelevant*);			//远离食物方案B
+int fnFarthestToTheSnakeTail(SnakeRelevant*);			//远离蛇尾方案
 
 
 //Tools
-int ManHaadun_x(int, int);
-int ManHaadun_y(int, int);
-int TrunSnake_byPlayer(int);
-void MoveSnake_Head(SnakeMap*, int*, int*);
-void MoveSnake_Tail(SnakeMap*, int*);
-int _condition(int);
-void SpeedSnake(bool, SnakeRelevant*);
+int ManHaadun_x(int, int);								//两点竖直距离
+int ManHaadun_y(int, int);								//两点水平距离
+int TrunSnake_byPlayer(int);							//通过玩家转向
+void MoveSnake_Head(SnakeMap*, int*, int*);				//移动蛇头
+void MoveSnake_Tail(SnakeMap*, int*);					//移动蛇尾
+int getSquareSafetyFactor(int);							//获取方块周边通行率
+void SpeedSnake(bool, SnakeRelevant*);					//截停
 
 #endif // !_SKANE_H
