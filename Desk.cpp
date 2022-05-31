b01ab8e0ea315676bdcf60eaffb29c51271b1334
winingYang <98200894+winingYang@users.noreply.github.com>
2022-05-31 15:36:36 +0800

    Update Desk.cpp

--- Desk.cpp
+++ Desk.cpp
@@ -10,40 +10,40 @@ char SELECT() {
 
 void DEBUGAlpha(bool op) {
 	int i = 0;
-	double ageCompleteness = 0;
-	while (i++ < 3000) {
-		InitGame();
+	double agedebuggingResults = 0;
+	while (i++ < 100) {
+		InitMsp();
 		while (CheakSnakeSafety()) {
 			MoveSnake();
 			TrunSnake(true, op);
 		}
-		ageCompleteness += completeness();
-		printf("\n\tNo.%-4d %%%3.1lf", i, 100 * completeness());
-		if (i % 10 == 0) {
+		agedebuggingResults += debuggingResults();
+		printf("\n\tNo.%-4d %%%3.1lf", i, 100 * debuggingResults());
+		if (i % 5 == 0) {
 			//print();
-			printf("\n\n\tAge: %%%4.2lf", ageCompleteness / i * 100);
+			printf("\n\n\tAge: %%%4.2lf", agedebuggingResults / i * 100);
 			if (op) {
-				DrewSnake(1);
-				Sleep(1000);
+				fnDisplaySnake(1);
+				Sleep(500);
 				if (_kbhit())
 					system("pause");
 			}
 		}
 	}
-	printf("\n\n\n\n\tAge: %%%4.2lf", ageCompleteness / (i - 1) * 100);
+	printf("\n\n\n\n\tAge: %%%4.2lf", agedebuggingResults / (i - 1) * 100);
 	system("pause");
 }
 
-void SnakeGame(bool isHuman_AI) {
+void GameStart(bool isHuman_AI) {
 
-	InitGame();
+	InitMsp();
 	while (CheakSnakeSafety())
 	{
 		MoveSnake();
-		DrewSnake(isHuman_AI);
+		fnDisplaySnake(isHuman_AI);
 		TrunSnake(isHuman_AI, 1);
 	}
-	DrewSnake(isHuman_AI);
+	fnDisplaySnake(isHuman_AI);
 	system("pause");
 }
 
